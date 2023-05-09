7519cc743984e8c3d8d984e1e016037239c791c0
Carbon225 <carbon225@protonmail.com>
2023-05-10 01:21:49 +0200

    login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,15 @@
 export default function Login() {
   return (
-    <div>
-      Login Screen
+    <div className="flex flex-col justify-center items-center my-40">
+      <div className="rounded-xl bg-base-300 p-5 flex flex-col justify-start items-start">
+        <div className="self-center text-2xl font-bold mb-3">Log in</div>
+        <input type="text" placeholder="Email" className="input input-bordered mb-3" />
+        <input type="password" placeholder="Password" className="input input-bordered mb-3" />
+        <div className="self-stretch flex flex-row justify-between items-start">
+          <button className="btn btn-link">Register</button>
+          <button className="btn btn-primary">Log in</button>
+        </div>
+      </div>
     </div>
   )
 }
