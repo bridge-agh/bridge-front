386cbea1b0d85302b307a7de05d62a121b938850
Carbon225 <carbon225@protonmail.com>
2023-05-10 03:07:52 +0200

    register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,14 @@
 export default function Register() {
   return (
-    <div>
-      Register Screen
+    <div className="flex flex-col justify-center items-center my-20">
+      <div className="rounded-xl bg-base-300 p-5 flex flex-col justify-start items-center">
+        <div className="text-2xl font-bold mb-3">Create account</div>
+        <input type="text" placeholder="Username" className="input input-bordered mb-3" />
+        <input type="text" placeholder="Email" className="input input-bordered mb-3" />
+        <input type="password" placeholder="Password" className="input input-bordered mb-3" />
+        <input type="password" placeholder="Confirm password" className="input input-bordered mb-3" />
+        <button className="btn btn-primary">Create account</button>
+      </div>
     </div>
   )
 }
