0d8dc73486c5be2a2423c7d303689a5ff0f686e1
Szymon Idec <szymon.idec@protonmail.com>
2023-05-11 20:21:33 +0200

    theme switch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Drawer from '@/components/root/drawer/drawer'
 import Header from '@/components/root/header/header'
 import '@/styles/global.css'
 import { Inter } from 'next/font/google'
+import Providers from '@/app/providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,14 +16,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <Header />
-        <Drawer>
-          <div className="main grid grid-cols-4 sm:grid-cols-6 lg:grid-cols-8 xl:grid-cols-12 p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12">
-            {children}
-          </div>
-        </Drawer>
+        <Providers>
+          <Header />
+          <Drawer>
+            <div className="main grid grid-cols-4 sm:grid-cols-6 lg:grid-cols-8 xl:grid-cols-12 p-4 sm:p-6 md:p-8 lg:p-10 xl:p-12">
+              {children}
+            </div>
+          </Drawer>
+        </Providers>
       </body>
     </html>
   )
--- src/components/root/header/header.tsx
+++ src/components/root/header/header.tsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
 import DrawerButton from "@/components/root/drawer/drawer_button";
+import ThemeSwitch from "@/components/theme_switch";
 
 export default function Header() {
+
+
     return (
         <header className="navbar bg-base-300 p-4 header">
             <div className="flex-1 navbar-title">
@@ -18,15 +21,15 @@ export default function Header() {
 
                 {/* post-login */}
                 <div className="hidden md:inline-flex dropdown dropdown-end">
-                    
+
                     <label tabIndex={0} className="btn btn-ghost btn-circle bg-neutral-focus w-16 h-16">
-                        <span className="w-10 rounded-full">
+                        <span className="w-10 text-neutral-content rounded-full">
                             YOU
                         </span>
                     </label>
                     <ul tabIndex={0} className="mt-16 p-2 shadow menu menu-compact dropdown-content bg-base-200 rounded-box w-52">
                         <li><a>Profile</a></li>
-                        <li><a>Settings</a></li>
+                        <li><ThemeSwitch className="self-stretch" /></li>
                         <li><a>Logout</a></li>
                     </ul>
                 </div>
