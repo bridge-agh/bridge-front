fb700ba9d5afb80797eacdf2cf05547e835d83d5
Szymon Idec <szymon.idec@protonmail.com>
2023-05-13 02:24:34 +0200

    home color fix

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="col-start-1 col-span-4 sm:col-start-2 sm:col-span-4 lg:col-start-3 lg:col-span-4 xl:col-start-5 xl:col-span-4">
       <div className="flex flex-col border-opacity-50">
         <div className="h-auto card bg-base-200 rounded-box place-items-center p-6 gap-6">
-          <p className="text-3xl text-secondary-content text-center">Join a game</p>
+          <p className="text-3xl text-accent-content text-center">
             Join a game
           </p>
           <div className="flex flex-col sm:flex-row w-full gap-4">
@@ -20,8 +20,7 @@ export default function Home() {
         </div>
         <div className="divider">OR</div>
         <div className="h-auto card bg-base-200 rounded-box place-items-center p-6 gap-6">
-          <p className="text-3xl text-secondary-content text-center">Create your lobby</p>
-          <Link href="/lobby" className="btn btn-primary">Create a game</Link>
+          <p className="text-3xl text-accent-content text-center">
             Create your lobby
           </p>
           <Link href="/lobby" className="btn btn-primary">
