2a4f0d7ab6784ae2aced16ed5792bc8fefca4e1c
Carbon225 <carbon225@protonmail.com>
2023-06-14 14:38:22 +0200

    remove analytics

--- src/logic/fb.tsx
+++ src/logic/fb.tsx
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
@@ -14,6 +13,5 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
