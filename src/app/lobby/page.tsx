d05e33ecf6fb62615d3938ac8741bc89882bc214
Carbon225 <carbon225@protonmail.com>
2023-10-05 13:12:10 +0200

    ready

--- src/app/lobby/page.tsx
+++ src/app/lobby/page.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import { useCallback } from "react";
+import { useRouter } from "next/navigation";
+import { useCallback, useEffect } from "react";
 import { useFindSession } from "@/api/session";
 import { useGetLobby, useLeaveLobby, useReady } from "@/api/session/lobby";
 import protectRoute from "@/logic/protect_route";
 import useUserUid from "@/logic/use_user_uid";
 
-function Player({ name }: { name: string }) {
+function Player({ name, ready }: { name: string, ready: boolean }) {
   return (
     <div className="flex flex-row pe-2 rounded-3xl justify-start items-center bg-base-100">
-      <div className="w-14 h-14 rounded-full bg-blue-600 me-3 flex flex-col justify-center items-center">
+      <div className={`w-14 h-14 rounded-full ${ready ? "bg-green-600" : "bg-blue-600"} me-3 flex flex-col justify-center items-center`}>
         <div>YOU</div>
       </div>
       <div className="text-lg font-bold">{name.slice(0, 8)}...</div>
@@ -18,6 +19,7 @@ function Player({ name }: { name: string }) {
 }
 
 function Lobby() {
+  const router = useRouter();
   const { uid: userUid } = useUserUid();
   const findSession = useFindSession(userUid);
   const getLobby = useGetLobby(findSession.data?.session_id);
@@ -39,7 +41,13 @@ function Lobby() {
     setReady.trigger({ user_id: userUid, session_id: findSession.data.session_id });
   }, [setReady, findSession, userUid]);
 
-  if (findSession.loading || getLobby.loading) {
+  useEffect(() => {
+    if (getLobby.data !== undefined && getLobby.data.users.length === 4 && getLobby.data.ready.every((ready) => ready)) {
+      router.push("/game");
+    }
+  }, [router, getLobby]);
+
+  if (userUid === undefined || findSession.loading || getLobby.loading) {
     return <div>Loading...</div>;
   }
 
@@ -55,6 +63,7 @@ function Lobby() {
   }
 
   const lobby = getLobby.data;
+  const myIndex = lobby.users.indexOf(userUid);
 
   return (
     <div className="col-start-1 col-span-4 sm:col-start-2 sm:col-span-4 lg:col-start-3 lg:col-span-4 xl:col-start-5 xl:col-span-4">
@@ -62,12 +71,12 @@ function Lobby() {
         <div className="text-2xl font-bold mb-3 self-center">Lobby</div>
         <div className="flex flex-row justify-between items-center mb-3">
           <div className="flex flex-col justify-start items-stretch me-16 gap-4">
-            <Player name={lobby.users[0] || "Waiting..."} />
-            <Player name={lobby.users[1] || "Waiting..."} />
+            <Player name={lobby.users[0] || "Waiting..."} ready={lobby.ready[0]} />
+            <Player name={lobby.users[1] || "Waiting..."} ready={lobby.ready[1]} />
           </div>
           <div className="flex flex-col justify-start items-stretch gap-4">
-            <Player name={lobby.users[2] || "Waiting..."} />
-            <Player name={lobby.users[3] || "Waiting..."} />
+            <Player name={lobby.users[2] || "Waiting..."} ready={lobby.ready[2]} />
+            <Player name={lobby.users[3] || "Waiting..."} ready={lobby.ready[3]} />
           </div>
         </div>
         <div className="flex flex-row justify-between items-center">
@@ -77,7 +86,7 @@ function Lobby() {
           <button className="btn btn-primary" onClick={handleCopyClick}>
             Copy ID
           </button>
-          <button className="btn btn-primary" onClick={handleReadyClick}>
+          <button className="btn btn-primary" disabled={lobby.ready[myIndex]} onClick={handleReadyClick}>
             Ready
           </button>
         </div>
