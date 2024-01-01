edf2a97c91c1c2a0f0a57ab4fd3156bb38b0c720
karolsliwa <karol.sliwa01@wp.pl>
2024-01-01 19:44:57 +0100

    show declarer and trump

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { useSessionInfo } from "@/api/session";
-import { GameStage, PlayerDirection, getPlayerDirectionName } from "@/game_engine/gameModels";
+import { PlayerDirection, getPlayerDirectionName } from "@/game_engine/gameModels";
 import protectRoute from "@/logic/protect_route";
 import useUser from "@/logic/use_user";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { twMerge } from "tailwind-merge";
+import { suitToSymbol } from "../game/bidding/biddingBids";
 
 function Results() {
 
@@ -15,8 +16,6 @@ function Results() {
 
   if (!sessionInfo.data || !user) return null;
 
-  // if (!sessionInfo.data.gameState) router.push("/lobby");
-  // if (sessionInfo.data.started && sessionInfo.data.gameState?.base.game_stage != GameStage.SCORING) router.push("/game");
 
   const users = sessionInfo.data?.users;  
   const player = users.find(u => u.id == user.user?.uid);
@@ -25,30 +24,41 @@ function Results() {
   if (player!.position in [PlayerDirection.NORTH, PlayerDirection.SOUTH]) result = sessionInfo.data.gameState?.game.tricks.NS.length;
   else result = sessionInfo.data.gameState?.game.tricks.NS.length; 
 
-  if (!result) result = 0;
+  const bid = sessionInfo.data.gameState?.bidding.bid;
+
+  const teamDirections = (directions: PlayerDirection[], reverse: boolean) => {
+    return (
+      <div className="flex flex-col items-center justify-between w-[95%] md:w-[45%]" key={directions.map(p => p.toString()).join()}>
+        {directions.map(direction => (
+          <div className={twMerge("flex", reverse ? "flex-row-reverse" : "flex-row", "items-center justify-between w-[95%] ms:w-[75%]")} key={direction}>
+            <div className={twMerge("text-accent-content text-sm xs:text-base font-bold", reverse ? "ml-8" : "mr-8")}>{getPlayerDirectionName(direction)}</div>
+            <div className="font-semibold xs:font-bold truncate ">
+              {users.filter(u => u.position == direction).map(u => u.id)}
+            </div>
+          </div>
+        ))}                
+      </div>
+    );
+  };
 
   return (                                   
     <div className="col-start-1 col-span-4 sm:col-start-2 sm:col-span-4 md:col-start-1 md:col-span-6 lg:col-start-2 lg:col-span-6 xl:col-start-4 xl:col-span-6">
       <div className="rounded-xl bg-base-200 p-5 flex flex-col justify-start items-stretch">
-        <div className="text-2xl font-bold mb-5 self-center">{result >= 7 ? "Victory!" : "Defeat"}</div>
+        <div className="text-2xl font-bold mb-5 text-center">{result ? (result >= 7 ? "Victory!" : "Defeat") : "Playing"}</div>
         <div className="flex flex-col md:flex-row justify-center items-center w-[100%] mb-6">        
-          {[[PlayerDirection.NORTH, PlayerDirection.SOUTH], [], [PlayerDirection.EAST, PlayerDirection.WEST]].map(positions => {
-            if (positions.length == 0) return <div className="text-2xl font-bold self-center" key={0}>VS</div>;
-            else return (
-              <div className="flex flex-col items-center justify-between w-[95%] md:w-[50%]" key={positions.map(p => p.toString()).join()}>
-                {positions.map(position => (
-                  <div className="flex flex-row items-center justify-between w-[75%]" key={position}>
-                    <div className="text-accent-content text-sm xs:text-base font-bold mr-8">{getPlayerDirectionName(position)}</div>
-                    <div className="font-semibold xs:font-bold truncate ">
-                      {users.filter(u => u.position == position).map(u => u.id)}
-                    </div>
-                  </div>
-                ))}                
-              </div>
-            );
-          })}
+          {teamDirections([PlayerDirection.NORTH, PlayerDirection.SOUTH], false)}
+          <div className="text-2xl font-bold self-center my-6" key={0}>VS</div>
+          {teamDirections([PlayerDirection.EAST, PlayerDirection.WEST], true)}
         </div>
+        <div className="text-lg font-bold mb-6 self-center">Declarer: {sessionInfo.data.gameState?.bidding.declarer}</div> 
 
+        <div className="flex flex-row gap-1 bid-history transition-all mb-6 text-lg self-center">
+          <span className="font-bold">Trump:</span>
+          <span>{bid?.tricks}</span>
+          {bid ? <div className="w-6 h-6 flex flex-col justify-center fill-neutral">
+            {suitToSymbol(bid.suit)}
+          </div> : ""}
+        </div>
 
         <div className="text-2xl font-bold mb-8 self-center">Won tricks: {result}/13</div>
         <div className="flex flex-row justify-center items-center">
